6c2b39ea2130dc190737d37fab23463c33dfca81
Gonçalo Morais <gnclmorais@gmail.com>
2015-06-26 21:41:01 +0100

    UI works start.
    After getting Foursquare’s access token, start async requests
    to fill our app with pretty stuff.

--- routes/places.js
+++ routes/places.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-  app.render('places', {
+  res.render('places', {
     name: 'Tobi'
   });
 });
